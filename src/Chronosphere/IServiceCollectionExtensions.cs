8a907d1ce256a722a183cddcfc04dea24d452bac
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-15 02:18:01 -0600

    Creates a single system clock instance type that implements every interface instead of one each
    This makes it so that you can cast the type to any of the clocks, and it makes it less work to create the types

--- src/Chronosphere/IServiceCollectionExtensions.cs
+++ src/Chronosphere/IServiceCollectionExtensions.cs
@@ -10,7 +10,7 @@ namespace Chronosphere
 {
     public static class IServiceCollectionExtensions
     {
-        public static IServiceCollection AddChronosphere(this IServiceCollection serviceCollection) => 
+        public static IServiceCollection AddChronosphere(this IServiceCollection serviceCollection) =>
             AddChronosphere(serviceCollection, DateTimeOffset.UtcNow, Assembly.GetCallingAssembly());
 
         public static IServiceCollection AddChronosphere(this IServiceCollection serviceCollection, DateTimeOffset now) =>
@@ -23,71 +23,67 @@ namespace Chronosphere
         {
             serviceCollection.AddScoped<IChronosphere>(x => new Chronosphere(now));
             serviceCollection.AddScoped<IReadOnlyChronosphere>(x => x.GetRequiredService<IChronosphere>());
-            serviceCollection.AddScoped<ISystemClock, SystemClock>();
 
-            foreach (var systemClock in BuildSystemClocks(rootAssembly))
-                serviceCollection.AddScoped(systemClock.InterfaceType, systemClock.InstanceType);
+            var systemClockInterfaces = GetSystemClockInterfaces(rootAssembly);
+
+            var systemClockInstanceType = BuildSystemClockInstanceType(systemClockInterfaces);
+
+            foreach (var systemClockInterfaceType in systemClockInterfaces)
+                serviceCollection.AddScoped(systemClockInterfaceType, systemClockInstanceType);
 
             return serviceCollection;
-        }
 
-        private static IEnumerable<(Type InterfaceType, Type InstanceType)> BuildSystemClocks(Assembly rootAssembly)
-        {
-            var assemblies = new Stack<Assembly>(new[] { rootAssembly });
-            
-            var assembliesLookup = assemblies.ToHashSet();
-
-            while(assemblies.TryPop(out var assembly))
+            static IEnumerable<Type> GetSystemClockInterfaces(Assembly rootAssembly)
             {
-                foreach (var referencedAssemblyName in assembly.GetReferencedAssemblies())
+                var assemblies = new Stack<Assembly>(new[] { rootAssembly });
+
+                var assembliesLookup = assemblies.ToHashSet();
+
+                while (assemblies.TryPop(out var assembly))
                 {
-                    try
+                    foreach (var referencedAssemblyName in assembly.GetReferencedAssemblies())
                     {
-                        var referencedAssembly = Assembly.Load(referencedAssemblyName);
+                        try
+                        {
+                            var referencedAssembly = Assembly.Load(referencedAssemblyName);
 
-                        if (assembliesLookup.Contains(referencedAssembly)) continue;
+                            if (assembliesLookup.Contains(referencedAssembly)) continue;
 
-                        assembliesLookup.Add(referencedAssembly);
-                        assemblies.Push(referencedAssembly);
+                            assembliesLookup.Add(referencedAssembly);
+                            assemblies.Push(referencedAssembly);
+                        }
+                        catch (FileNotFoundException) { }
                     }
-                    catch (FileNotFoundException) { }
                 }
+
+                return assembliesLookup.SelectMany(x => x.GetTypes())
+                    .Where(x =>
+                    {
+                        if (x.Name != "ISystemClock") return false;
+                        if (!x.IsInterface) return false;
+                        var members = x.GetMembers();
+                        if (!members.All(x =>
+                            x is MethodInfo mx && mx.Name == "get_UtcNow" && mx.ReturnType == typeof(DateTimeOffset)
+                            || x is PropertyInfo px && px.Name == "UtcNow" && px.PropertyType == typeof(DateTimeOffset)
+                        )) return false;
+                        return true;
+                    })
+                    .Append(typeof(ISystemClock))
+                    .AsEnumerable();
             }
 
-            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName($"Chronosphere_Assembly_{Guid.NewGuid()}"), AssemblyBuilderAccess.Run);
-            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule($"Chronosphere_Module_{Guid.NewGuid()}");
-
-            var baseCtor = typeof(SystemClock).GetConstructors().Single();
-
-            var systemClockTypes = assembliesLookup.SelectMany(x => x.GetTypes())
-                .Where(x =>
-                {
-                    if (x.Name != "ISystemClock") return false;
-                    if (!x.IsInterface) return false;
-                    if (x == typeof(ISystemClock)) return false;
-                    var members = x.GetMembers();
-                    if (!members.All(x =>
-                        x is MethodInfo mx && mx.Name == "get_UtcNow" && mx.ReturnType == typeof(DateTimeOffset)
-                        || x is PropertyInfo px && px.Name == "UtcNow" && px.PropertyType == typeof(DateTimeOffset)
-                    )) return false;
-                    return true;
-                })
-                .Select(x =>
-                {
-                    var instanceType = BuildSystemClockInstanceType(x);
-                    return (InterfaceType: x, InstanceType: instanceType);
-                })
-                .ToArray();
-
-            return systemClockTypes;
-
-            Type BuildSystemClockInstanceType(Type interfaceType)
+            static Type BuildSystemClockInstanceType(IEnumerable<Type> systemClockInterfaceTypes)
             {
+                var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName($"Chronosphere_Assembly_{Guid.NewGuid()}"), AssemblyBuilderAccess.Run);
+                ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule($"Chronosphere_Module_{Guid.NewGuid()}");
+
+                var baseCtor = typeof(SystemClock).GetConstructors().Single();
+
                 var typeBuilder = moduleBuilder.DefineType(
                     $"Chronosphere_SystemClock_{Guid.NewGuid()}",
                     TypeAttributes.Public,
                     typeof(SystemClock),
-                    new Type[] { interfaceType, typeof(ISystemClock) }
+                    systemClockInterfaceTypes.ToArray()
                 );
 
                 var ctorBuilder = typeBuilder.DefineConstructor(
