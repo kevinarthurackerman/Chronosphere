d23f7e52f421249b9a3945549bd874f5406f6ce6
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-16 03:56:39 -0600

    Modifies nuspec file

--- src/Chronosphere/IServiceCollectionExtensions.cs
+++ src/Chronosphere/IServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
