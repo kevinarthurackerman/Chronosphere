6ecbc8bcf29e984d83f058283c41f5b9bb855596
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-28 20:33:53 -0600

    Updates to deploy nuget package

---
+++ scripts/variables.ps1
@@ -0,0 +1,11 @@
+$majorVersion=1
+$minorVersion=0
+$buildVersion=2
+$postfixVersion="-beta"
+$version="$majorVersion.$minorVersion.$buildVersion$postfixVersion"
+
+$packageId="Chronosphere"
+$projectPath="..\src\Chronosphere\Chronosphere.csproj"
+$authors="Kevin Ackerman"
+$copyright="MIT"
+$repoUrl="https://github.com/kevinarthurackerman/Chronosphere"
